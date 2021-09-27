5e49d6842e36178ff4f8acb86df6bfef8e031a3a
VegardOztan <vegard.oztan@gmail.com>
2021-09-27 09:37:24 +0200

    Fix cypress test according to last commit

--- frontend/cypress/integration/welcome_page_spec.ts
+++ frontend/cypress/integration/welcome_page_spec.ts
@@ -3,9 +3,7 @@ import { users } from '../support/mock/users'
 describe('Test Welcome page', () => {
     before(() => {
         const user = users[0]
-        cy.login(user)
-        const frontendUrl = Cypress.env('FRONTEND_URL') || 'http://localhost:3000'
-        cy.visit(frontendUrl)
+        cy.visitProject(user)
     })
     it('Buttons exist', () => {
         cy.contains('Order').should('be.visible')
---
+++ frontend/cypress/support/mock/projects.ts
@@ -0,0 +1,37 @@
+import * as faker from 'faker'
+
+export class FusionProject {
+    id: string
+    name: string
+
+    constructor({ id, name }: { id: string; name: string }) {
+        this.id = id
+        this.name = name
+    }
+}
+
+export const fusionProject1 = new FusionProject({
+    id: '123',
+    name: 'Banana Banana',
+})
+
+export const fusionProjects = [fusionProject1]
+
+export function findFusionProjectByID(id: string) {
+    return fusionProjects.filter(p => p.id == id)[0]
+}
+
+export function getFusionProjectData(project: FusionProject) {
+    return {
+        id: project.id,
+        externalId: 'M.O0000.00.0.0000',
+        source: null,
+        type: { id: 'Project', isChildType: true, parentTypeIds: ['ProjectMaster'] },
+        value: { wbs: 'M.O0000.00.0.0000', projectMasterId: null, isValid: faker.datatype.boolean() },
+        title: project.name,
+        isActive: faker.datatype.boolean(),
+        isDeleted: faker.datatype.boolean(),
+        created: '2020-09-01T20:20:00.00+00:00',
+        updated: '2020-10-01T20:20:00.00+00:00',
+    }
+}
