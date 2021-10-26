d97d9b4340a25ee8ffc237b0797d948bb4335b5f
VegardOztan <vegard.oztan@gmail.com>
2021-10-26 09:09:34 +0200

    Relocate using statements

--- backend/src/Controllers/TestController.cs
+++ backend/src/Controllers/TestController.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
-
     [ApiController]
     [Route("/")]
     public class TestController : ControllerBase
