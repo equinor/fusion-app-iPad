569c3c506d19215519432e918ed48a0048fa6b12
VegardOztan <vegard.oztan@gmail.com>
2021-10-28 08:45:45 +0200

    Run prettier

--- frontend/src/Return.tsx
+++ frontend/src/Return.tsx
@@ -1,28 +1,29 @@
-import { Banner } from "@equinor/eds-core-react"
+import { Banner } from '@equinor/eds-core-react'
 import { Button } from '@equinor/fusion-components'
-
 
 const Return = () => {
     return (
         <>
             <Banner>
-                <Banner.Message>
-                    To return an iPad, please use the service-now solution by clicking the button below.
-                </Banner.Message>
+                <Banner.Message>To return an iPad, please use the Service-Now solution by clicking the button below.</Banner.Message>
                 <Banner.Actions placement="bottom">
-                    <Button url={"https://equinor.service-now.com/selfservice?id=sc_cat_item&sys_id=18f643a36f51f900a7c62dc71e3ee437"}>Return</Button>
+                    <Button url={'https://equinor.service-now.com/selfservice?id=sc_cat_item&sys_id=18f643a36f51f900a7c62dc71e3ee437'}>
+                        Return
+                    </Button>
                 </Banner.Actions>
             </Banner>
             <Banner>
                 <Banner.Message>
-                    To change ownership of an iPad, please use the service-now solution by clicking the button below and selecting the dropdown-option "Change ownership of iPad".
+                    To change ownership of an iPad, please use the Service-Now solution by clicking the button below and selecting the
+                    dropdown-option "Change ownership of iPad".
                 </Banner.Message>
                 <Banner.Actions placement="bottom">
-                    <Button url={"https://equinor.service-now.com/selfservice?id=sc_cat_item&sys_id=886323f7dbca2f00855b9d51ba961911"}>Change ownership</Button>
+                    <Button url={'https://equinor.service-now.com/selfservice?id=sc_cat_item&sys_id=886323f7dbca2f00855b9d51ba961911'}>
+                        Change ownership
+                    </Button>
                 </Banner.Actions>
             </Banner>
         </>
-
     )
 }
 
