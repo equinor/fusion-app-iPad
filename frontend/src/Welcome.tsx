cc86952c465f60f7dfea536896786f5de038d40d
Torstein <57716654+GodVenn@users.noreply.github.com>
2021-10-29 09:45:54 +0100

    Link to return service now return form (#53)
    * Add links to service now solution for return and change ownership
    
    * Change front page to use banner
    
    * Upgrade Cypress
    
    * Set base image to LTS-alpine
    
    This change is due to problems with the node:alpine image, causing the frontend docker to fail
    
    Co-authored-by: VegardOztan <vegard.oztan@gmail.com>
    Co-authored-by: Maia Tømmerbakk <maito@equinor.com>

--- frontend/src/Welcome.tsx
+++ frontend/src/Welcome.tsx
@@ -1,3 +1,4 @@
+import { Banner } from '@equinor/eds-core-react'
 import { Button } from '@equinor/fusion-components'
 import { RouteComponentProps } from 'react-router-dom'
 
@@ -9,11 +10,13 @@ const Welcome = ({ match }: RouteComponentProps<Params>) => {
     const fusionProjectId = match.params.fusionProjectId
 
     return (
-        <div>
-            <Button relativeUrl={`${fusionProjectId}/order`}> Order </Button>
-            <Button relativeUrl={`${fusionProjectId}/support`}> Support </Button>
-            <Button relativeUrl={`${fusionProjectId}/return`}> Return </Button>
-        </div>
+        <Banner>
+            <Banner.Actions>
+                <Button relativeUrl={`${fusionProjectId}/order`}> Order </Button>
+                <Button relativeUrl={`${fusionProjectId}/support`}> Support </Button>
+                <Button relativeUrl={`${fusionProjectId}/return`}> Return </Button>
+            </Banner.Actions>
+        </Banner>
     )
 }
 
