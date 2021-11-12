443e1715ea64b7cea9624dbd753d5a5cf56b77d7
VegardOztan <vegard.oztan@gmail.com>
2021-11-12 10:34:31 +0100

    Add hooks.ts file

---
+++ frontend/src/utils/hooks.ts
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import { useApiClients, Context } from '@equinor/fusion'
+
+import { PositionDetails } from '../api/models'
+import { getName, getValidPosition } from './helpers'
+
+export const useValidPositionsAsync = (
+    setValidPositions: (newPositions: PositionDetails[]) => void,
+    setHasFetched: (newState: boolean) => void,
+    currentContext: Context | null
+) => {
+    const apiClients = useApiClients()
+    const positionNames = ['CC Manager', 'Construction Manager', 'Commissioning Manager', 'Project Manager', 'Project Director']
+    useEffect(() => {
+        //Filter out correct positions. map id, name and assigned person and corresponding name. Finally filter invalid elements
+        if (currentContext?.externalId) {
+            apiClients.org.getPositionsAsync(currentContext.externalId).then(response => {
+                setValidPositions(
+                    response.data
+                        .filter(position => positionNames.some(substring => position.name.includes(substring)))
+                        .map(
+                            (position): PositionDetails => ({
+                                positionId: position.id,
+                                positionName: position.name,
+                                assignedPerson: getValidPosition(position.instances)?.assignedPerson,
+                                assignedPersonName: getName(getValidPosition(position.instances)),
+                            })
+                        )
+                        .filter(element => element.assignedPerson !== undefined && element.assignedPerson !== null)
+                )
+                setHasFetched(true)
+            })
+        }
+    }, [currentContext])
+}
