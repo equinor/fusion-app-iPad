9617405f556a4c9a69d40aa76b70af7943a1d0e9
Torstein Lundervold Nesheim <TLNE@equinor.com>
2021-12-07 16:40:24 +0100

    Add loading spinner when submitting

--- frontend/src/components/SubmitFormDialog.tsx
+++ frontend/src/components/SubmitFormDialog.tsx
@@ -1,4 +1,5 @@
 import { Button, Dialog, Typography } from '@equinor/eds-core-react'
+import { Spinner } from '@equinor/fusion-components'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -12,23 +13,34 @@ const Wrapper = styled.div`
 interface SubmitFormDialogProps {
     ritm: string
     onConfirmClick: () => void
+    isLoading: boolean
 }
 
-export const SubmitFormDialog = ({ ritm, onConfirmClick }: SubmitFormDialogProps) => {
+export const SubmitFormDialog = ({ ritm, onConfirmClick, isLoading }: SubmitFormDialogProps) => {
     return (
-        <Dialog data-testid={'submit_dialog'}>
-            <Dialog.Title>Order successful!</Dialog.Title>
-            <Dialog.CustomContent>
-                <Typography variant="body_short">
-                    The order was submitted successfully to Service Now. The RITM for your order is
-                </Typography>
-                <Typography variant="body_short_bold">{ritm}</Typography>
-            </Dialog.CustomContent>
-            <Dialog.Actions>
-                <Wrapper>
-                    <Button onClick={onConfirmClick}>OK</Button>
-                </Wrapper>
-            </Dialog.Actions>
-        </Dialog>
+        <>
+            {isLoading ? (
+                <Dialog data-testid={'submit_dialog'}>
+                    <Dialog.CustomContent>
+                        <Spinner centered={true} />
+                    </Dialog.CustomContent>
+                </Dialog>
+            ) : (
+                <Dialog data-testid={'submit_dialog'}>
+                    <Dialog.Title>Order successful!</Dialog.Title>
+                    <Dialog.CustomContent>
+                        <Typography variant="body_short">
+                            The order was submitted successfully to Service Now. The RITM for your order is
+                        </Typography>
+                        <Typography variant="body_short_bold">{ritm}</Typography>
+                    </Dialog.CustomContent>
+                    <Dialog.Actions>
+                        <Wrapper>
+                            <Button onClick={onConfirmClick}>OK</Button>
+                        </Wrapper>
+                    </Dialog.Actions>
+                </Dialog>
+            )}
+        </>
     )
 }
