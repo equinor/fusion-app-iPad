a223f82d40d9a4754fbfd98c49988e13206dea53
Torstein Lundervold Nesheim <TLNE@equinor.com>
2021-12-09 15:41:41 +0100

    Add function to log to cypress output console

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -19,4 +19,12 @@
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
+
+    // Task to log to node console
+    on('task', {
+        log (message) {
+          console.log(message)
+          return null
+        }
+      })
 }
