ebc4781f07d0a182c780802103eddde66ff18878
VegardOztan <vegard.oztan@gmail.com>
2021-12-21 13:59:07 +0100

    Make jest work with react

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    verbose: true,
+    testEnvironment: 'jsdom',
+    transform: {
+        '.(js)': 'babel-jest',
+        '.(ts|tsx)': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/?!(@equinor/fusion)'],
+    setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
 }
---
+++ frontend/setupJest.ts
@@ -0,0 +1,10 @@
+import 'jest-canvas-mock'
+const crypt = require('crypto')
+
+declare var window: Window & typeof globalThis
+
+Object.defineProperty(window.self, 'crypto', {
+    value: {
+        getRandomValues: (arr: any[]) => crypt.randomBytes(arr.length),
+    },
+})
