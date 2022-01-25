1cb4d4a0138113ece2f5bdc3c2e220298d05dba0
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-01-25 14:24:50 +0100

    Add workflow for migration notification

--- backend/api/Database/Migrations/20220125132348_TestNewMigration.Designer.cs
+++ backend/api/Database/Migrations/20220125132348_TestNewMigration.Designer.cs
@@ -3,6 +3,7 @@ using Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220125132348_TestNewMigration")]
+    partial class TestNewMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/api/Database/Migrations/20220125132348_TestNewMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Database.Migrations
+{
+    public partial class TestNewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "YellowTag",
+                table: "IPads",
+                type: "nvarchar(140)",
+                maxLength: 140,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "YellowTag",
+                table: "IPads",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(140)",
+                oldMaxLength: 140);
+        }
+    }
+}
