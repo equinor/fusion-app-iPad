df707808a87ac364ab91aaea3c4b72939d7a4ead
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-02-02 12:38:17 +0100

    Add Database Access Layer

---
+++ backend/api/Database/IPadDatabaseAccess.cs
@@ -0,0 +1,24 @@
+﻿using Api.Database.Entities;
+using Api.Database.Models;
+using Api.Utilities;
+
+namespace Api.Database
+{
+    /// <summary>
+    /// This represents our Data Access Layer
+    /// </summary>
+    public class IPadDatabaseAccess
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public IPadDatabaseAccess(DatabaseContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<PagedList<IPad>> GetIPads(IPadParameters iPadParameters)
+        {
+            return await PagedList<IPad>.ToPagedList(_dbContext.IPads.Select(iPad => iPad), iPadParameters.PageNumber, iPadParameters.PageSize);
+        }
+    }
+}
