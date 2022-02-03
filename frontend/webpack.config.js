5de1aaf04c0619bc1b47e1f47a8ffeeebab29a50
VegardOztan <vegard.oztan@gmail.com>
2022-02-03 16:02:37 +0100

    Remove hardcoded ClientId from webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,7 +9,7 @@ if (process.env.BUILD_MODE && process.env.BUILD_MODE === 'production') {
 const mode = isProduction ? 'production' : 'development'
 
 const API_URL = process.env.API_URL
-const AD_CLIENT_ID = process.env.AD_CLIENT_ID || 'd83c2116-0a79-4a0d-8276-d51cdb4a6fd6'
+const AD_CLIENT_ID = process.env.AD_CLIENT_ID
 const AD_TENANT_ID = process.env.AD_TENANT_ID || '3aa4a235-b6e2-48d5-9195-7fcf05b459b0'
 const FUSION_CONTEXT_KEY = '74b1613f-f22a-451b-a5c3-1c9391e91e68'
 
