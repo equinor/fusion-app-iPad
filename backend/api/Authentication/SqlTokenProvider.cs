c6e1a7eafbe5c6370de52cc5d7bac8c682d99985
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-02-08 15:26:24 +0100

    Add more logging

--- backend/api/Authentication/SqlTokenProvider.cs
+++ backend/api/Authentication/SqlTokenProvider.cs
@@ -12,15 +12,18 @@ namespace Api.Authentication
     {
         private readonly ClientSecretCredential _credentials;
         private static readonly TokenRequestContext tokenRequest = new(new[] { "https://database.windows.net/.default" });
+        private readonly ILogger<SqlTokenProvider> _logger;
 
-        public SqlTokenProvider(IConfiguration config)
+        public SqlTokenProvider(IConfiguration config, ILogger<SqlTokenProvider> logger)
         {
+            _logger = logger;
             string clientId = config.GetValue<string>("AzureAd:ClientId");
             string clientSecret = config.GetValue<string>("AzureAd:ClientSecret");
 
             string? migration = Environment.GetEnvironmentVariable("PERFORM_MIGRATION");
             if (migration is not null && string.Equals(migration, "true", StringComparison.OrdinalIgnoreCase))
             {
+                _logger.LogWarning("'PERFORM_MIGRATION' set to 'true' - Using database owner credentials");
                 clientId = config.GetValue<string>("AzureAd:DatabaseOwner:ClientId");
                 clientSecret = config.GetValue<string>("AzureAd:DatabaseOwner:ClientSecret");
             }
@@ -34,7 +37,9 @@ namespace Api.Authentication
 
         public async Task<string> GetAccessTokenAsync()
         {
+            _logger.LogInformation("Getting database access token");
             var token = await _credentials.GetTokenAsync(tokenRequest);
+            _logger.LogInformation("Successfully retrieved database access token");
             return token.Token;
         }
     }
