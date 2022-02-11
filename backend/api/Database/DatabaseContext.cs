ba71b2615e176d1769466372f2acd1308f6f2a9d
Torstein Lundervold Nesheim <TLNE@equinor.com>
2022-02-11 15:40:10 +0100

    Auto-update datetimes

--- backend/api/Database/DatabaseContext.cs
+++ backend/api/Database/DatabaseContext.cs
@@ -11,5 +11,26 @@ namespace Api.Database
 
         public DbSet<IPad> IPads => Set<IPad>();
 
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is IPad && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((IPad)entityEntry.Entity).ModifiedAt = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((IPad)entityEntry.Entity).CreatedAt = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
     }
 }
